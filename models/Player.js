db3c502ba4d2b1504c2cc0ce5e9a8eb4d9659321
Brandon <brandon@Brandons-MacBook-Pro-70.local>
2024-12-16 22:07:02 -0500

    map

--- models/Player.js
+++ models/Player.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  playerName: { type: String, required: true }, // Changed to playerName
+  playerName: { type: String, required: true }, 
   birthYear: { type: Number },
-  positions: { type: [String], default: [] },  // An array of strings
-  citizenship: { type: [String], default: [] },  // An array of strings
+  positions: { type: [String], default: [] },  // Array of strings
+  citizenship: { type: [String], default: [] },  
   currentAvailability: { type: String },
   experience: { type: Number },
   profileImage: { type: String },
-  playerCV: { type: String }
+  playerCV: { type: String },
+  highlightVideo: { type: String }, // Add this field
+  fullMatchVideo: { type: String }, // Add this field
+  email: { type: String },          // Add this field
+  whatsapp: { type: String },       // Add this field
+  agentEmail: { type: String }      // Add this field
 });
 
 module.exports = mongoose.model('Player', playerSchema);
